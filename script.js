d791e561322664daa7703016390c55950b345dbe
Subhro <shaswata.ssaha@gmail.com>
2022-11-20 00:01:30 +0530

    changes saved

---
+++ script.js
@@ -0,0 +1,168 @@
+// Women Ethnic
+let pop = document.getElementById("popup");
+const wom = document.getElementById("womeneth");
+
+wom.addEventListener("mouseenter",()=> {
+    pop.classList.remove("hide");
+});
+pop.addEventListener("mouseenter",()=> {
+    pop.classList.remove("hide");
+});
+pop.addEventListener("mouseleave",()=> {
+    pop.classList.add("hide");
+});
+
+wom.addEventListener("mouseleave",()=> {
+    pop.classList.add("hide");
+});
+
+// Women western
+let pop2 = document.getElementById("popup2");
+const womw = document.getElementById("womenwest");
+
+womw.addEventListener("mouseenter",()=> {
+    pop2.classList.remove("hide");
+});
+pop2.addEventListener("mouseenter",()=> {
+    pop2.classList.remove("hide");
+});
+pop2.addEventListener("mouseleave",()=> {
+    pop2.classList.add("hide");
+});
+
+womw.addEventListener("mouseleave",()=> {
+    pop2.classList.add("hide");
+});
+
+
+// Men
+let pop3 = document.getElementById("popup3");
+const men = document.getElementById("men");
+
+men.addEventListener("mouseenter",()=> {
+    pop3.classList.remove("hide");
+});
+pop3.addEventListener("mouseenter",()=> {
+    pop3.classList.remove("hide");
+});
+pop3.addEventListener("mouseleave",()=> {
+    pop3.classList.add("hide");
+});
+
+men.addEventListener("mouseleave",()=> {
+    pop3.classList.add("hide");
+});
+
+
+// Kids
+let pop4 = document.getElementById("popup4");
+const kid = document.getElementById("kid");
+
+kid.addEventListener("mouseenter",()=> {
+    pop4.classList.remove("hide");
+});
+pop4.addEventListener("mouseenter",()=> {
+    pop4.classList.remove("hide");
+});
+pop4.addEventListener("mouseleave",()=> {
+    pop4.classList.add("hide");
+});
+
+kid.addEventListener("mouseleave",()=> {
+    pop4.classList.add("hide");
+});
+
+
+// Home & Kitchen
+let pop5 = document.getElementById("popup5");
+const kit = document.getElementById("kit");
+
+kit.addEventListener("mouseenter",()=> {
+    pop5.classList.remove("hide");
+});
+pop5.addEventListener("mouseenter",()=> {
+    pop5.classList.remove("hide");
+});
+pop5.addEventListener("mouseleave",()=> {
+    pop5.classList.add("hide");
+});
+
+kit.addEventListener("mouseleave",()=> {
+    pop5.classList.add("hide");
+});
+
+
+// Beauty & Health
+let pop6 = document.getElementById("popup6");
+const bea = document.getElementById("bea");
+
+bea.addEventListener("mouseenter",()=> {
+    pop6.classList.remove("hide");
+});
+pop6.addEventListener("mouseenter",()=> {
+    pop6.classList.remove("hide");
+});
+pop6.addEventListener("mouseleave",()=> {
+    pop6.classList.add("hide");
+});
+
+bea.addEventListener("mouseleave",()=> {
+    pop6.classList.add("hide");
+});
+
+
+// Jewellery & Accessories
+let pop7 = document.getElementById("popup7");
+const jew = document.getElementById("jew");
+
+jew.addEventListener("mouseenter",()=> {
+    pop7.classList.remove("hide");
+});
+pop7.addEventListener("mouseenter",()=> {
+    pop7.classList.remove("hide");
+});
+pop7.addEventListener("mouseleave",()=> {
+    pop7.classList.add("hide");
+});
+
+jew.addEventListener("mouseleave",()=> {
+    pop7.classList.add("hide");
+});
+
+
+// Jewellery & Accessories
+let pop8 = document.getElementById("popup8");
+const bag = document.getElementById("bag");
+
+bag.addEventListener("mouseenter",()=> {
+    pop8.classList.remove("hide");
+});
+pop8.addEventListener("mouseenter",()=> {
+    pop8.classList.remove("hide");
+});
+pop8.addEventListener("mouseleave",()=> {
+    pop8.classList.add("hide");
+});
+
+bag.addEventListener("mouseleave",()=> {
+    pop8.classList.add("hide");
+});
+
+
+// Jewellery & Accessories
+let pop9 = document.getElementById("popup9");
+const ele = document.getElementById("elec");
+
+ele.addEventListener("mouseenter",()=> {
+    pop9.classList.remove("hide");
+});
+pop9.addEventListener("mouseenter",()=> {
+    pop9.classList.remove("hide");
+});
+pop9.addEventListener("mouseleave",()=> {
+    pop9.classList.add("hide");
+});
+
+ele.addEventListener("mouseleave",()=> {
+    pop9.classList.add("hide");
+});
